1a6510b88c2861bd1adc1f8567a2411250b43b33
Mone Venkatesulu <venkatesulumone15@gmail.com>
2025-09-29 20:01:36 +0000

    Remove extra tabs container and Update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,75 +11,66 @@ const Header = () => {
   const {updateSearchedMovie} = useContext(SearchedMovieContext)
   const pathName = location.pathname
 
-  const linksContainer = (
-    <ul className="header-link-container">
-      <li>
-        <Link
-          to="/"
-          className="header-link-item"
-          style={{fontWeight: `${pathName === '/' ? 'bold' : 'normal'}`}}
-        >
-          Popular
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/top-rated"
-          className="header-link-item"
-          style={{
-            fontWeight: `${pathName === '/top-rated' ? 'bold' : 'normal'}`,
-          }}
-        >
-          Top Rated
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/upcoming"
-          className="header-link-item"
-          style={{
-            fontWeight: `${pathName === '/upcoming' ? 'bold' : 'normal'}`,
-          }}
-        >
-          Upcoming
-        </Link>
-      </li>
-    </ul>
-  )
-
   return (
-    <>
-      <nav className="nav-contianer responsive-padding">
+    <nav className="nav-contianer responsive-padding">
+      <div className="header-title-link-container">
         <h1 className="app-title">movieDB</h1>
 
-        <div className="tabs-container-sm">{linksContainer}</div>
-
-        <div>
-          <input
-            type="text"
-            placeholder="Movie Name"
-            className="searchbar"
-            value={userSearch}
-            onChange={e => setUserSearch(e.target.value)}
-            onKeyDown={e =>
-              e.key === 'Enter' && updateSearchedMovie(userSearch)
-            }
-          />
-          <Link to="/searched-movies">
-            <button
-              type="button"
-              onClick={() => updateSearchedMovie(userSearch)}
+        <ul className="header-link-container">
+          <li>
+            <Link
+              to="/"
+              className="header-link-item"
+              style={{fontWeight: `${pathName === '/' ? 'bold' : 'normal'}`}}
             >
-              Search
-            </button>
-          </Link>
-        </div>
-      </nav>
-
-      <div className="tabs-container-mobile responsive-padding">
-        {linksContainer}
+              Popular
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/top-rated"
+              className="header-link-item"
+              style={{
+                fontWeight: `${pathName === '/top-rated' ? 'bold' : 'normal'}`,
+              }}
+            >
+              Top Rated
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/upcoming"
+              className="header-link-item"
+              style={{
+                fontWeight: `${pathName === '/upcoming' ? 'bold' : 'normal'}`,
+              }}
+            >
+              Upcoming
+            </Link>
+          </li>
+        </ul>
       </div>
-    </>
+
+      <div className="searchbar-container">
+        <input
+          type="text"
+          placeholder="Movie Name"
+          className="searchbar"
+          value={userSearch}
+          onChange={e => setUserSearch(e.target.value)}
+          onKeyDown={e => e.key === 'Enter' && updateSearchedMovie(userSearch)}
+        />
+        <Link to="/searched-movies">
+          <button
+            type="button"
+            onClick={() => updateSearchedMovie(userSearch)}
+            className="search-btn"
+          >
+            Search
+          </button>
+        </Link>
+      </div>
+    </nav>
   )
 }
 export default Header
